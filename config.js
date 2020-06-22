d71a43d00a022435e4f7f752ee0057a5cde4a960
neil-oliver <neiloliverphoto@gmail.com>
2020-06-22 10:14:46 -0400

    recommendation implementation
    highest ranked coin recommendation system in place and Pair exclusion list implemented.

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 module.exports = {
   verbose:false,
   intervals:['1m','5m','15m','30m','1h','6h','12h','1d','1w'],
-  rankings:['priceChange','priceChangePercent','volume','count'],
+  rankings:['priceChange','priceChangePercent','buySellRatio','volume','trades'],
   exchanges: ["Binance"],
-  preferredBase: "USDT",
+  exclusionList:['BCHSVUSDT'], // add any symbols of pairs that should not be included.
+  updateExclusions:true, // recheck and remove items in the exclusion list on update
+  preferredQuote: "USDT",
   saveType: "file", // file or db
   repeatMenu: true,
 }
