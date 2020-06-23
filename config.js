b5616d9a3d45f2ef867c3920c5af5f05ed2d07e7
neil-oliver <neiloliverphoto@gmail.com>
2020-06-23 00:35:21 -0400

    arbitrage v0.1

--- config.js
+++ config.js
@@ -1,11 +1,14 @@
 module.exports = {
   verbose:false,
-  intervals:['1m','5m','15m','30m','1h','6h','12h','1d','1w'],
+  intervals:['1m'], //['1m','5m','15m','30m','1h','6h','12h','1d','1w']
   rankings:['priceChange','priceChangePercent','buySellRatio','volume','trades'],
   exchanges: ["Binance"],
   exclusionList:['BCHSVUSDT'], // add any symbols of pairs that should not be included.
   updateExclusions:true, // recheck and remove items in the exclusion list on update
   preferredQuote: "USDT",
+  onlyPreferredQuote:false,
+  arbitrageTrusted:["BTC","USDT","ETH","LTC","XRP","BCH","NANO","XLM", "GDP"],
+  arbitrageTrustedOnly:true,
   saveType: "file", // file or db
   repeatMenu: true,
 }
