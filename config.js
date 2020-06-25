1da3dfe6f74a5d8d6c4d10e6a7c770a74bfc9ded
neil-oliver <neiloliverphoto@gmail.com>
2020-06-24 22:15:22 -0400

    live updates v0.1

--- config.js
+++ config.js
@@ -1,14 +1,22 @@
 module.exports = {
   verbose:false,
-  intervals:['1m'], //['1m','5m','15m','30m','1h','6h','12h','1d','1w']
+  intervals:['1m','5m','1h','12h'], //['1m','5m','15m','30m','1h','6h','12h','1d','1w']
   rankings:['priceChange','priceChangePercent','buySellRatio','volume','trades'],
   exchanges: ["Binance"],
   exclusionList:['BCHSVUSDT'], // add any symbols of pairs that should not be included.
   updateExclusions:true, // recheck and remove items in the exclusion list on update
   preferredQuote: "USDT",
-  onlyPreferredQuote:false,
+  onlyPreferredQuote:true,
   arbitrageTrusted:["BTC","USDT","ETH","LTC","XRP","BCH","NANO","XLM", "GDP"],
-  arbitrageTrustedOnly:true,
+  arbitrageTrustedOnly:false,
   saveType: "file", // file or db
   repeatMenu: true,
+  fees:{ // not available through API
+    Binance:{
+      maker: 0.001,
+      taker: 0.001
+    }
+  },
+  hot:5 // 24 'high' hour percentage 
+
 }
